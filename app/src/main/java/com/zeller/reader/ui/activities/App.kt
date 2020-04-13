f3f467129b864b684ebbf9df685c9ba625e10205
Zellerpooh <zellerpooh@gmail.com>
2020-04-13 17:43:29 +0800

    initial commit

---
+++ app/src/main/java/com/zeller/reader/ui/activities/App.kt
@@ -0,0 +1,11 @@
+package com.zeller.reader.ui.activities
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/zeller/reader/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zeller.reader.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeller.reader.R.layout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
